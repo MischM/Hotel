52b4a1c1ccde5743123a362fae9fe0d306e79447
Misch <mi.meyer@hotmail.ch>
2017-01-31 09:29:53 +0100

    hotel class

---
+++ Hotel/Hotel/Models/Hotel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hotel.Models
+{
+    public class Hotel
+    {
+        private int hotelID;
+        private string description;
+        private int stars;
+
+        //Constructors
+        public Hotel(int id, string desc, int stars)
+        {
+             
+        }
+    }
+}
